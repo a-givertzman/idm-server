ba9edc9b706b749fa73115c551fdb987d88d299f
Anton Lobanov <a.givertzman@icloud.com>
2025-05-05 19:07:32 +0300

    Server | Initial

--- src/server/cot.rs
+++ src/server/cot.rs
@@ -1,13 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 ///
-/// Cose of transmission
+/// Cose of transmission of the TCP message
 /// - `Act` - Activation
 /// - `ActCon` - Activation confirmation
-/// - `Inf` - Information
+/// - `ActErr` - Activation error
+/// - `Rec` - Recquest
+/// - `RecCon` - Request confirmation, contains reply
+/// - `RecErr` - Request error
+/// - `Inf` - Information message
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Cot {
     Act,
     ActCon,
+    Req,
+    ReqCon,
     Inf,
 }
