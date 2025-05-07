ebfa67b2ff3847f64a4ffd7c2714a6985b223761
Anton Lobanov <a.givertzman@icloud.com>
2025-05-07 15:44:23 +0300

    Server | DeviceInfo test

--- src/server/cot.rs
+++ src/server/cot.rs
@@ -2,13 +2,13 @@ use serde::{Deserialize, Serialize};
 
 ///
 /// Cose of transmission of the TCP message
-/// - `Act` - Activation
-/// - `ActCon` - Activation confirmation
-/// - `ActErr` - Activation error
-/// - `Rec` - Recquest
-/// - `RecCon` - Request confirmation, contains reply
-/// - `RecErr` - Request error
-/// - `Inf` - Information message
+/// - `Act` - Activation (Client -> Server)
+/// - `ActCon` - Activation confirmation (Server -> Client)
+/// - `ActErr` - Activation error (Server -> Client)
+/// - `Rec` - Recquest (Client -> Server)
+/// - `RecCon` - Request confirmation, contains reply (Server -> Client)
+/// - `RecErr` - Request error (Server -> Client)
+/// - `Inf` - Information message (Server -> Client)
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Hash)]
 pub enum Cot {
     Act,
